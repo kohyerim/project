54d706f4d5bf769ca543e4be3ceb649a24869345
koheyrim <kohyerim78@gmail.com>
2017-12-08 16:35:57 +0900

    update

--- Screen.py
+++ Screen.py
@@ -7,7 +7,7 @@ pygame.display.init()
 pygame.init()
 pygame.font.init()
 
-FONT = pygame.font.Font('resources/CALIBRATE1.ttf', 50)
+FONT = pygame.font.Font('resources/CALIBRATE1.ttf', 25)
 
 
 class Screen:
@@ -26,17 +26,26 @@ class Screen:
                   fish_img.get_image(48, 48 * 2, 48, 48),
                   fish_img.get_image(48 * 2, 48 * 2, 48, 48)]
     fish_now = fish_frame[0]
+    whale_img = Image('resources/whale_sprite.png')
+    whale_frame = [whale_img.get_image(0, 0, 100, 100),
+                   whale_img.get_image(100, 0, 100, 100),
+                   whale_img.get_image(200, 0, 100, 100)]
+    whale_now = whale_frame[0]
     background = pygame.image.load('resources/background.jpg')
     menu_background = pygame.image.load('resources/menu_background.jpg')
     over_background = pygame.image.load('resources/game-over background_Resized.png')
     food_background = pygame.image.load('resources/food_back-resized.jpg')
     enemy_background = pygame.image.load('resources/enemy-resized.jpg')
     back_button = pygame.image.load('resources/button-back-Resized.png')
+    start_button = pygame.image.load('resources/button-start-game3.png')
+    replay_button = pygame.image.load('resources/button-play-again-resized.png')
+    life = Image('resources/heart_Resized2.png')  # 라이프 이미지 불러오기
+    life_list = [life.get_image(0, 0, 36, 44),  # 꽉 찬 하트
+                 life.get_image(36, 0, 36, 44)]  # 빈 하트
 
     def __init__(self):
         pygame.display.set_caption("Game")
         self.screen = pygame.display.set_mode((self.width, self.height))
-        self.screen.blit(self.menu_background, (0, 0))
         self.penguin_x = 800
         self.penguin_y = 300
         self.wait = True
@@ -69,19 +78,19 @@ class Screen:
                 self.current_frame = (self.current_frame + 1) % len(self.penguin_frame)
                 self.image = self.penguin_move_frame[self.current_frame]
 
-    def food_animate(self, frame):
+    def animate(self, frame):
         now = pygame.time.get_ticks()
-        if now - self.last_update > 100:
+        if now - self.last_update > 150:
             self.last_update = now
             self.current_frame = (self.current_frame + 1) % len(self.fish_frame)
         return frame[self.current_frame]
 
     def menu(self):
         start = False
-        start_button = pygame.image.load('resources/button-start-game3.png')
 
         while not start:
-            start_rect = self.screen.blit(start_button, (350, 400))
+            self.screen.blit(self.menu_background, (0, 0))
+            start_rect = self.screen.blit(self.start_button, (350, 400))
             pygame.display.update()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -102,6 +111,8 @@ class Screen:
             self.penguin_animate(penguin_state, penguin_move)
             self.screen.blit(self.background, (0, 0))
             self.screen.blit(self.image, (self.penguin_x, self.penguin_y))
+            self.text("SCORE  :  ", FONT, (0, 0, 0), 60, 10)
+            self.text(str(self.score), FONT, (0, 0, 0), 130, 10)
             food_button_rect = self.screen.blit(food_button, (900, 500))
             if penguin_move and self.penguin_x > 320:
                 self.penguin_x -= 3
@@ -128,31 +139,38 @@ class Screen:
                         penguin_move = True
                         self.num = random.randint(0, 10)
                         print("랜덤값 : %d " % self.num)
+            if self.heart == 3:
+                self.screen.blit(self.life_list[0], (10 + 850, 10))
+                self.screen.blit(self.life_list[0], (50 + 850, 10))
+                self.screen.blit(self.life_list[0], (90 + 850, 10))
+            elif self.heart == 2:
+                self.screen.blit(self.life_list[0], (10 + 850, 10))
+                self.screen.blit(self.life_list[0], (50 + 850, 10))
+                self.screen.blit(self.life_list[1], (90 + 850, 10))
+            elif self.heart == 1:
+                self.screen.blit(self.life_list[0], (10 + 850, 10))
+                self.screen.blit(self.life_list[1], (50 + 850, 10))
+                self.screen.blit(self.life_list[1], (90 + 850, 10))
             pygame.display.update()
 
     def food(self):
         finish = False
-        penguin_x = 200
-        penguin_y = 200
-        fish_list = []
+        penguin = pygame.transform.flip(self.penguin_move_frame[0], True, False)
+        penguin_x = 10
+        penguin_y = self.height/2
         fish_rect_list = []
-        count = random.randint(10, 15)
+        count = random.randint(10, 20)
         for i in range(0, count):
-            fish = Fish(self.screen, self.food_animate(self.fish_frame), random.randint(0, 300), random.randint(50, 500), self.fish_frame)
-            fish_list.append(fish)
+            fish = Fish(self.screen, self.animate(self.fish_frame), random.randint(70, 300), random.randint(50, 500), self.fish_frame)
             fish_rect_list.append(fish)
 
         while not finish:
-            penguin = pygame.transform.flip(self.penguin_move_frame[0], True, False)
             self.screen.blit(self.food_background, (0, 0))
             penguin_rect = self.screen.blit(penguin, (penguin_x, penguin_y))
-            back_button_rect = self.screen.blit(self.back_button, (20, 20))
+            back_button_rect = self.screen.blit(self.back_button, (850 + 20, 20))
             self.text("FOOD", FONT, (0, 0, 0), self.width/2, self.height/4)
-
-            for fish in fish_list:
-                rect = fish.move()
-                fish_rect_list.pop(0)
-                fish_rect_list.append(rect)
+            self.text("SCORE  :  ", FONT, (0, 0, 0), 60, 10)
+            self.text(str(self.score), FONT, (0, 0, 0), 130, 10)
 
             pressed = pygame.key.get_pressed()
             if pressed[pygame.K_UP]:
@@ -164,10 +182,11 @@ class Screen:
             if pressed[pygame.K_RIGHT]:
                 if penguin_x < 1000 - 48:
                     penguin_x += 3
+                    penguin = pygame.transform.flip(self.penguin_move_frame[0], True, False)
             if pressed[pygame.K_LEFT]:
                 if -10 < penguin_x:
                     penguin_x -= 3
-
+                    penguin = self.penguin_move_frame[0]
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     exit(0)
@@ -178,32 +197,42 @@ class Screen:
             for fish in fish_rect_list:
                 if penguin_rect.colliderect(fish):
                     self.score += 5
-                    print(self.score)
-
+                    fish_rect_list.remove(fish)
+                fish.move()
             pygame.display.update()
 
     def enemy(self):
         finish = False
-        test = pygame.Surface((50, 50))
-        test.fill((255, 255, 0))
         self.heart -= 1
-        print("남은 생명 : %d" % self.heart)
+        penguin = pygame.transform.flip(self.penguin_move_frame[0], True, False)
+        penguin_x = 280
+        penguin_y = self.height/2
+        whale_x = 10
+        penguin_rect = self.screen.blit(penguin, (280, self.height / 2))
+        back_button_rect = None
         while not finish:
             pygame.display.update()
             self.screen.blit(self.enemy_background, (0, 0))
+            whale = self.animate(self.whale_frame)
+            whale_rect = self.screen.blit(whale, (whale_x, penguin_y - 30))
+            whale_x += 2.05
+            penguin_x += 1.55
             self.text("ENEMY", FONT, (0, 0, 0), self.width / 2, self.height / 4)
-            test_rect = self.screen.blit(test, (100, 500))
             if self.heart <= 0:
                 finish = True
+            if not whale_rect.colliderect(penguin_rect):
+                penguin_rect = self.screen.blit(penguin, (penguin_x, penguin_y))
+            if whale_x >= 990:
+                back_button_rect = self.screen.blit(self.back_button, (850 + 20, 20))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     exit(0)
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    if test_rect.collidepoint(event.pos):
+                    if back_button_rect.collidepoint(event.pos):
                         finish = True
 
     def choice(self, num):
-        if num >= 7:
+        if num >= 8:
             self.enemy()
         else:
             self.food()
@@ -212,15 +241,10 @@ class Screen:
         self.score = 0
         self.heart = 3
         over = False
-        replay_button = pygame.Surface((50, 50))
-        replay_button.fill((15, 80, 98))
-        gameover_button = pygame.Surface((50, 50))
-        gameover_button.fill((30, 25, 55))
         while not over:
             self.screen.blit(self.over_background, (0, 0))
             self.text("GAME OVER", FONT, (0, 0, 0), self.width / 2, self.height / 4)
-            replay_button_rect = self.screen.blit(replay_button, (400, 400))
-            gameover_button_rect = self.screen.blit(gameover_button, (300, 400))
+            replay_button_rect = self.screen.blit(self.replay_button, (850 + 20, 20))
             pygame.display.update()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -228,15 +252,14 @@ class Screen:
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     if replay_button_rect.collidepoint(event.pos):
                         over = True
-                    elif gameover_button_rect.collidepoint(event.pos):
-                        exit(0)
 
 
 game = Screen()
-game.menu()
+
 play = True
 while True:
     play = True
+    game.menu()
     while play:
         game.start()
         game.choice(game.num)
